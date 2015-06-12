ecec6c01fc5d498e236f363d5dd5ff6a640e8e6d
Tim Knight <timmilesknight@gmail.com>
2015-06-13 00:04:50 +0100

    Completed and commented task

---
+++ cipher.rb
@@ -0,0 +1,30 @@
+# Task:
+# Implement a caesar cipher that takes in a string 
+# and the shift factor and then outputs the modified string:
+# 		> caesar_cipher("What a string!", 5)
+#     => "Bmfy f xywnsl!"
+
+
+def cipher(string, shift)
+
+#Loop through the string
+	string.each_char do |letter|	
+
+	#Keep spaces as spaces rather than being shifted
+		if letter.ord == 32
+			print " "
+	#For A-Z loop from Z to A
+		elsif ((letter.ord + shift).between?(91,96))
+				print (letter.ord-26+shift).chr
+	#For a-z loop from z to a
+		elsif (letter.ord + shift  >= 122)
+			print (letter.ord-25+shift).chr
+	#If letter doesn't need looping		
+		else
+			print (letter.ord+shift).chr
+		end
+	end
+end
+
+cipher("What a string!", 5)
+=>"Bmfy f xywnsl!"
